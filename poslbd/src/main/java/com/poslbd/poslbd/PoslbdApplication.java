6956cb418bc235e95d963a595adae9770a15857f
WaltRG <walter75321@hotmail.com>
2024-11-12 11:38:07 -0600

    Commint de prueba

---
+++ poslbd/src/main/java/com/poslbd/poslbd/PoslbdApplication.java
@@ -0,0 +1,13 @@
+package com.poslbd.poslbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslbdApplication.class, args);
+	}
+
+}
---
+++ poslbd/src/test/java/com/poslbd/poslbd/PoslbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poslbd.poslbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoslbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
